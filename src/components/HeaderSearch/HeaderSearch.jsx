2ffd1d1d484fd132927aa4c0c860bbea017ba7b5
Muhammad Mehboob <muhammadmehboobz7869@gmail.com>
2025-08-24 22:18:49 +0500

    Update HeaderSearch.jsx

--- src/components/HeaderSearch/HeaderSearch.jsx
+++ src/components/HeaderSearch/HeaderSearch.jsx
@@ -7,18 +7,18 @@ function HeaderSearch(){
       <div className="container-xxl">
         <div className="row header-row">
           <div className="col logo">
-            <img src="/images/logo_140x40_crop_center@2x.png" alt="Logo" />
+            <img src="images/logo_140x40_crop_center@2x.png" alt="Logo" />
           </div>
           <div className="col search">
             <form onSubmit={(e)=> e.preventDefault()}>
               <input type="text" placeholder="Search" />
               <button>
-                <img src="/images/logo.svg" alt="Search"/>
+                <img src="images/logo.svg" alt="Search"/>
               </button>
             </form>
           </div>
           <div className="col cart">
-            <img src="/images/cart.svg" alt="Cart"/>
+            <img src="images/cart.svg" alt="Cart"/>
             <span>Cart</span>
           </div>
         </div>
